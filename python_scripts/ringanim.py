cfc91fe9dd629d6d633efad049316ceeae64f02e
erikleitch <eleitch@basho.com>
2016-09-22 11:34:40 -0700

    First-round debugged animation committed

--- python_scripts/ringanim.py
+++ python_scripts/ringanim.py
@@ -6,85 +6,296 @@ from matplotlib.patches import Polygon
 import profiler_util as pu
 import sys
 
+#=======================================================================
+# A file object
+#=======================================================================
+
+class File(object):
+
+    def __init__(self, fileName):
+
+        self.fileName = fileName
+
+        self.nFrameAdvance  = 0
+        self.nFrame         = 0
+        
+        f = open(fileName)
+
+        self.partitions = f.readline()
+        self.tags       = f.readline()            
+        
+        line     = f.readline()
+        counters = line.split(' ')
+
+        self.firstTimestamp = int((counters[0].split(':'))[0])
+        self.nLine = 1
+
+        for line in f:
+            self.nLine += 1
+
+        counters = line.split(' ')
+        self.lastTimestamp = int((counters[0].split(':'))[0])
+
+        print 'First time = ' + str(self.firstTimestamp) + ' last = ' + str(self.lastTimestamp) + ' nline = ' + str(self.nLine)
+        f.close()
+
+    def advanceStartFrame(self, nFrame):
+        self.nFrameAdvance += nFrame
+
+    def setNframe(self, nFrame):
+        self.nFrame = nFrame
+
+    def open(self):
+        self.f = open(self.fileName)
+
+        # And skip the first two lines
+        
+        self.f.readline()
+        self.f.readline()
+
+        # And advance by any requested number of frames
+
+        self.nLineRemaining = self.nLine
+        for i in range(0, self.nFrameAdvance):
+            self.f.readline()
+            self.nLineRemaining -= 1
+
+        # Set the remaining number of lines to the total, minus any lines we want to discard
+
+        if self.nFrame > 0 and self.nFrame < self.nLineRemaining:
+            self.nLineRemaining = self.nFrame
+
+    def close(self):
+        self.f.close()
+
+    def advanceToTimestamp(self, timeStamp):
+        self.advanceStartFrame((timeStamp - self.firstTimestamp) / 1000000)
+        
+    def truncateToTimestamp(self, timeStamp):
+        self.truncateEndFrame((self.lastTimestamp - timeStamp) / 1000000)
+
+    def getNextCounters(self):
+        line = self.f.readline()
+        counters = line.split(' ')
+        timesplit = counters[0].split(':')
+        timestamp = int(timesplit[0])
+
+        nitem = np.size(counters)
+
+        counters = counters[1:nitem]
+        self.nLineRemaining -= 1
+
+        return timestamp, counters
+
 class Cluster(object):
     
-    def __init__(self, ax, tag, fileNames):
-        self.nNode = np.size(fileNames)
-        self.nodes = []
-        self.ax = ax;
-        self.firstLine = True;
-        self.tag = tag
-        
-        ind = 1
-        for fileName in fileNames:
-            self.nodes.append(Node(ax, tag, fileName, ind))
-            ind += 1
+    def __init__(self, axes, tags, fileNames, skipstart, nFrame, floor):
 
-        self.totalNode = Node(ax, tag, fileNames[0], 0)
+        self.nNode     = np.size(fileNames)
+        self.nodes     = []
+        self.axes      = axes;
+        self.firstLine = True;
+        self.tags      = tags
+        self.ntag      = np.size(tags)
+        self.skipstart = skipstart
+        self.nFrame    = nFrame
+        self.files     = []
+        self.floor     = floor
+        self.firstFrame=True
         
+        for fileName in fileNames:
+            if fileName != '':
+                self.files.append(File(fileName))
+
+        firstCommonTimestamp = self.files[0].firstTimestamp
+        lastCommonTimestamp  = self.files[0].lastTimestamp
+
+        #------------------------------------------------------------
+        # Trim to the bounds where we have common timestamps
+        #------------------------------------------------------------
+        
+        for f in self.files:
+            
+            if f.firstTimestamp > firstCommonTimestamp:
+                firstCommonTimestamp = f.firstTimestamp
+
+            if f.lastTimestamp < lastCommonTimestamp:
+                lastCommonTimestamp = f.lastTimestamp
+
+        for f in self.files:
+            f.advanceStartFrame(skipstart)
+            f.setNframe(nFrame)
+            
+        #------------------------------------------------------------
+        # Now append nodes
+        #------------------------------------------------------------
+
+        # Outermost ring will have dR = 0.1 (1.0 - 0.9).  each
+        # successive ring will have radii that conserve the area of
+        # the outer ring
+        #
+        # dANp is the total conserved area that we want to match for
+        # each successive ring
+
+        outerRadius = 0.93
+        dANp = 4*np.pi * (1.0*1.0 - outerRadius*outerRadius)
+
+        self.totalNode = Node(axes, tags, self.files[0], 1.0, dANp)
+                
+        for f in self.files:
+            node = Node(axes, tags, f, outerRadius, dANp)
+            outerRadius = node.radiusa
+            self.nodes.append(node)
+
+        #------------------------------------------------------------
         # Get limits over all nodes
+        #------------------------------------------------------------
 
         self.getLimits()
 
+    #=======================================================================
+    # Update limits against values for the current line
+    #=======================================================================
+
     def getLimits(self):
         first = True
+        
         for node in self.nodes:
             if first:
                 first = False
-                self.nLine = node.nLine
-                self.tagLims = node.tagLims
-                self.tagTotals = node.tagTotals
-            else:
-                for key in self.tagLims.keys():
-                    print 'node = ' + str(node.fileName) + ' tags = ' + str(node.tagLims)
-                    print 'key  = ' + str(key) + ' totals = ' + str(node.tagTotals)
-                    self.tagTotals[key] += node.tagTotals[key]
+                self.nLine     = node.nLine
+                self.tagLims   = node.tagLims
 
-                    if node.tagLims[key] > self.tagLims[key]:
-                        self.tagLims[key] = node.tagLims[key]
+                self.totals = {}
+                for key in node.tagTotals.keys():
+                    self.totals[key] = np.zeros(node.nPart, dtype=np.int)
+
+                self.tagTotals = {}
+                for key in node.tagTotals.keys():
+                    self.tagTotals[key] = 0
+
+                self.tagTotalLims = {}
+                for key in node.tagTotals.keys():
+                    self.tagTotalLims[key] = 0
+
+            else:
+                if node.nLine < self.nLine:
+                    self.nLine = node.nLine
+
+            for key in self.tagLims.keys():
+                self.tagTotals[key] += node.tagTotals[key]
+
+                # Store the max totals over all nodes
                 
+                if np.max(node.tagTotals[key]) > self.tagTotalLims[key]:
+                    self.tagTotalLims[key] = np.max(node.tagTotals[key])
+
+                # Store the max instantaneous rates over all nodes
+
+                if node.tagLims[key] > self.tagLims[key]:
+                    self.tagLims[key] = node.tagLims[key]
+
+            if self.nFrame > 0 and self.nLine > self.nFrame:
+                self.nLine = self.nFrame
+
+    #=======================================================================
+    # Cluster::update is the main update method called during the animation
+    #=======================================================================
+    
     def update(self, val):
-        self.drawRing(val);
 
-    def drawRing(self, val):
-        if val == self.nLine-1:
+        # For some reason, the animation is called twice for the first frame, so ignore the first call
+        
+        if self.firstFrame:
+            self.firstFrame = False
             return
-        self.ax.clear()
-        plt.axis('off')
 
+        # If this isn't the last line (for which we just display
+        # totals), read the next line for all nodes
+
+        if val < self.nLine:
+            for node in self.nodes:
+                node.getNextCounters()
+
+        # And iterate over tags we are plotting
+        
+        for i in range(0, self.ntag):
+            self.drawRing(val, i)
+
+    #=======================================================================
+    # Cluster::drawRing calls down to Node::drawRing for all nodes
+    #=======================================================================
+    
+    def drawRing(self, val, tagInd):
+
+        tag = self.tags[tagInd]
+
+        if tagInd == 0:
+            print 'Frame: ' + str(("%4d" % val)) + ' (' + ("%3d" % (100*float(val)/self.nLine)) + '%) ts = ' + str(self.nodes[0].ts)
+
+        if val == self.nLine:
+            color = [0.3,0.5,1.0]
+            text = tag + '\n(cumulative)'
+        else:
+            color = [0.5,0.3,1.0]
+            text = tag
+
+        ccolor = [0.3,0.5,1.0]
+
+        ax = axes[tagInd]
+        ax.clear()
+        ax.axis('off')
+
+        #------------------------------------------------------------
+        # Now iterate over nodes, drawing the instantaneous records
+        #------------------------------------------------------------
+        
         for node in self.nodes:
-            node.drawRing(val, self.tagLims)
-            if self.firstLine:
-                self.totals = node.vals
-                self.firstLine = False
+
+            # If this is the last frame, display cumulative totals for all nodes
+            
+            if val == self.nLine:
+                node.drawRing(val, tagInd, self.tagLims, node.tagTotals[tag], self.tagTotalLims, self.floor, color)
+
+            # Else display the instantaneous count
+            
             else:
-                self.totals += node.vals
+                    
+                node.drawRing(val, tagInd, self.tagLims, None, None, self.floor, color)
+                if self.firstLine:
+                    self.totals[tag] = node.vals[tag]
+                    self.firstLine = False
+                else:
+                    self.totals[tag] += node.vals[tag]
 
-        self.totalNode.drawRing(val, self.tagLims, self.totals, self.tagTotals)
+        #------------------------------------------------------------
+        # Finally, draw the outermost ring with the cumulative totals
+        #------------------------------------------------------------
 
-        r = 0.5
-        g = 0.3
-        b = 1.0
+        self.totalNode.drawRing(val, tagInd, self.tagLims, self.totals[tag], self.tagTotals, self.floor, ccolor)
 
-        plt.text(0, 0, self.tag, color=[r,g,b], ha='center', size=18)
+        ax.text(0, 0, text, color=color, ha='center', size=18)
         
 class Node(object):
 
-    def __init__(self, ax, tag, fileName, ind):
-        self.ax = ax
-        self.ind = 0
-        self.fileName = fileName
-        self.radiusb = 1.0 - 0.1*ind
-        self.radiusa = self.radiusb - 0.1
+    def __init__(self, axes, tags, f, outerRadius, dANp):
+        self.axes = axes
+        self.f = f
         self.initializeRingSizeAndLimits()
-        self.tag = tag
+
+        # Outer radius for this ring is the specified start radius
+        # Inner radius is calculated to conserve area
         
+        self.radiusb = outerRadius
+        self.radiusa = np.sqrt(outerRadius*outerRadius  - dANp/(4*np.pi))
+        self.tags    = tags
+                                          
     def initializeRingSizeAndLimits(self):
 
-        self.f = open(self.fileName);
+        self.f.open()
 
-        partitionContent = self.f.readline()
-        tagContent = self.f.readline()
+        partitionContent = self.f.partitions
+        tagContent       = self.f.tags
 
         #------------------------------------------------------------
         # Count and order the partitions
@@ -96,8 +307,6 @@ class Node(object):
             self.partitionList.append(int(parts[i]))
             
         self.ps = np.argsort(self.partitionList)
-#        for i in range(0, np.size(self.ps)):
-#            print str(self.partitionList[self.ps[i]])
 
         self.nPart = np.size(self.ps)
 
@@ -111,41 +320,39 @@ class Node(object):
         self.tagInds = {}
         self.tagLims = {}
         self.tagTotals = {}
+        self.vals    = {}
         for i in range(1, np.size(tags)-1):
+            self.vals[tags[i]] = []
             self.tagList.append(tags[i])
             self.tagInds[tags[i]] = i-1
             self.tagLims[tags[i]] = 0
             self.tagTotals[tags[i]] = np.zeros(self.nPart, dtype=np.int)
-            print 'Found tag = ' + str(tags[i])
 
         #------------------------------------------------------------
         # Now read the rest of the file, and get the limits
         #------------------------------------------------------------
 
         self.nLine = 0
-        for line in self.f:
+
+        
+        for i in range(0, self.f.nLineRemaining):
+            timestamp, counters = self.f.getNextCounters()
             self.nLine += 1
-            counters = line.split(' ')
-            nitem = np.size(counters)
-            counters = counters[1:nitem]
             self.getLims(counters)
 
-#        for key in self.tagLims:
-#            print 'tag ' + key + ' has lim ' + str(self.tagLims[key])
-#            print 'tag ' + key + ' has totals ' + str(self.tagTotals[key])
+        self.f.close()
 
         # And reset the file descriptor
 
-        self.f = open(self.fileName);
-        self.f.readline()
-        self.f.readline()
+        self.f.open()
+
+        self.nLine = self.f.nLineRemaining
 
     def getLims(self, counters):
 
         # For each key, extract counters for all partitions
         
         for key in self.tagLims:
-            print 'key = ' + str(key) + ' fileName  = ' + str(self.fileName)
             vals = self.getOrderedCounts(counters, key)
             self.tagTotals[key] += vals
             if int(np.max(vals)) > int(self.tagLims[key]):
@@ -154,26 +361,30 @@ class Node(object):
     def getOrderedCounts(self, counters, key):
         vals = np.ndarray(self.nPart, dtype=np.int)
         tagInd = self.tagInds[key]
+
         for i in range(0, self.nPart):
             partInd  = self.ps[i]
             countInd = partInd * self.nTag + tagInd
             vals[i] = int(counters[countInd])
 
         return vals
-            
-    def drawRing(self, val, tagLims, inpVals=None, tagTotals=None):
 
-        line = self.f.readline()
+    def getNextCounters(self):
+        self.ts, self.counters = self.f.getNextCounters()
 
-        if line == '':
-            return
+    #=======================================================================
+    # Node::drawRing
+    #=======================================================================
+    
+    def drawRing(self, val, tagInd, tagLims, inpVals=None, tagTotals=None, floorval=0.0, rgb=[0.5,0.3,1.0]):
+
+        # If explicit input vals were passed, use those instead of reading from the file
+
+        tag = self.tags[tagInd]
+        ax  = self.axes[tagInd]
         
-        counters = line.split(' ')
-        nitem = np.size(counters)
-        counters = counters[1:nitem]
-
         if inpVals == None:
-            vals = self.getOrderedCounts(counters, self.tag)
+            vals = self.getOrderedCounts(self.counters, tag)
         else:
             vals = inpVals
 
@@ -181,11 +392,12 @@ class Node(object):
         # instantaneous max or the total max, depending on usage
         
         if inpVals != None:
-            tagMax = np.max(tagTotals[self.tag])
+            tagMax = np.max(tagTotals[tag])
         else:
-            tagMax = tagLims[self.tag]
+            tagMax = tagLims[tag]
         
         dtheta = (np.pi * 2) / self.nPart
+
 
         radiusa = self.radiusa
         radiusb = self.radiusb
@@ -206,16 +418,16 @@ class Node(object):
             patch = self.getPatch(theta, dtheta, 30)
 
             if vals[i] == 0:
-                mult = 0.1
+                mult = floorval
             else:
-                mult = float(vals[i]) / tagMax
+                mult = (float(vals[i]) / tagMax + floorval) / (1.0 + floorval)
 
-            r = 0.5 * mult
-            g = 0.3 * mult
-            b = 1.0 * mult
-            self.ax.add_patch(Polygon(patch, closed=True, facecolor=[r,g,b]))
+            fc = np.asarray(rgb) * mult
+            ec = np.asarray(rgb) * 0.7
 
-            self.vals = vals
+            ax.add_patch(Polygon(patch, closed=True, facecolor=fc, linewidth=0.3, edgecolor=ec))
+
+            self.vals[tag] = vals
         
     def getPatch(self, theta, dtheta, npt):                        
         patch = []
@@ -251,45 +463,69 @@ def getOptArgs(args, name, defval):
     else:
       return defval
 
+def getAxes(tags):
+
+    ntag = np.size(tags)
+    ns = int(np.sqrt(float(ntag)))
+    if ns == 1:
+        ny = 1
+        nx = ntag
+    else:
+        ny = ns
+        nx = np.ceil(float(ntag)/ns)
+
+    figsize=(8*nx,8*ny)
+    fig = plt.figure(figsize=figsize)
+
+    fig.set_facecolor('black');
+
+    axes = []
+    for i in range(0, ntag):
+        ax = fig.add_subplot(ny,nx,i+1)
+        ax.axis('off')
+        ax.set_aspect('equal', 'datalim')
+        ax.set_xlim([-1.2, 1.2])
+        ax.set_ylim([-1.2, 1.2])
+        axes.append(ax)
+
+    return fig, axes
+
 #=======================================================================
 # Main script
 #=======================================================================
-
-fig = plt.figure(figsize=(10,10))
-fig.set_facecolor('black');
-
-plt.axis('off')
-
-plt.xlim([-1,1])
-plt.ylim([-1,1])
-
-plt.axes().set_aspect('equal', 'datalim')
-
-ax   = plt.gca()
 
 #------------------------------------------------------------
 # Get optional args, and instantiate the Cluster object
 #------------------------------------------------------------
 
-files = getOptArgs(sys.argv, 'files',  'dev1_atomicCounters.txt dev2_atomicCounters.txt dev3_atomicCounters.txt')
-tag   = getOptArgs(sys.argv, 'tag',  'asyncput')
+files     = getOptArgs(sys.argv,       'files',     'dev1_atomicCounters.txt dev2_atomicCounters.txt dev3_atomicCounters.txt')
+tags      = getOptArgs(sys.argv,       'tags',      'syncput')
+skipstart = int(getOptArgs(sys.argv,   'skipstart', 0))
+nframe    = int(getOptArgs(sys.argv,   'nframe',    0))
+save      = bool(getOptArgs(sys.argv,  'save',      False))
+floor     = float(getOptArgs(sys.argv, 'floor',     0.0))
 
-ring = Cluster(ax, tag, files.split(' '))
+fig, axes = getAxes(tags.split(' '))
+
+ring = Cluster(axes, tags.split(' '), files.split(' '), skipstart, nframe, floor)
 
 #------------------------------------------------------------
 # And run the animation
 #------------------------------------------------------------
 
-anim = animation.FuncAnimation(fig, ring.update, ring.nLine, interval=10,
+print 'Running with ring.nLine = ' + str(ring.nLine)
+
+anim = animation.FuncAnimation(fig, ring.update, ring.nLine+1, interval=1,
                                blit=False, repeat=False)
 
 # Set up formatting for the movie files
 
-print 'Writers = ' + str(animation.writers)
+if save:
+    print 'Writers = ' + str(animation.writers)
 
-Writer = animation.writers['ffmpeg']
-writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
-
-anim.save('im.mp4', writer=writer)
+    Writer = animation.writers['ffmpeg']
+    writer = Writer(fps=5, metadata=dict(artist='Me'), bitrate=1800)
+    
+    anim.save('im.mp4', writer=writer, savefig_kwargs={'facecolor':'black'})
 
 plt.show()

b6fbee4667b335dd5c3691b0ff75a5b3c2213584
erikleitch <eleitch@basho.com>
2017-03-08 10:38:29 -0800

    Added plots from basho-perf

---
+++ python_scripts/intPlot.py
@@ -0,0 +1,518 @@
+import datetime, time
+import numpy as np
+import requests
+import matplotlib.pyplot as plt
+import sys
+import os
+import scipy
+import erltestutil as etu
+
+def smooth(y, box_pts):
+
+    n = np.size(y)
+    ntotal = 2*box_pts + n
+    print 'ntotal = ' + str(ntotal)
+    yarr = np.ndarray(ntotal,dtype=np.double)
+    print 'n = ' + str(n) + ' s1 = ' + str(np.shape(y)) + ' s2 = ' + str(np.shape(yarr))
+    
+    for i in range(0,ntotal):
+        if i < box_pts:
+            yarr[i] = 0.0
+        elif i >= n + box_pts:
+            yarr[i] = 0.0
+        else:
+            yarr[i] = y[i - box_pts]
+            
+    box = np.ones(box_pts)/box_pts
+    yarr_smooth = np.convolve(yarr, box, mode='same')
+    return yarr_smooth[box_pts:box_pts+n]
+            
+def gsmooth(x, y, sigma):
+    n = np.size(x)
+    s = np.ndarray(n, np.double)
+    for i in range(0, n):
+        dx = (x[i] - x[0])
+        s[i] = np.exp(-dx*dx/(2*sigma*sigma))
+    return s
+
+def retick(plt, ax):
+    xticks = ax.get_xticks()
+
+    newlabels = []
+    for label in xticks:
+        utc_dt = datetime.datetime.fromtimestamp(label)
+        newlabels.append(utc_dt.strftime("%H:%M:%S"))
+
+    print ' new = ' + str(newlabels)
+    ax.set_xticklabels(newlabels)
+    plt.setp( ax.xaxis.get_majorticklabels(), rotation=+0 )
+
+def relative(d):
+    if isinstance(d, dict):
+        for key in d.keys():
+            d[key]['x'] = relativeArr(d[key]['x'])
+    else:
+        return relativeArr(d)
+
+def relativeArr(x):
+    n = np.size(x)
+    x0 = x[0]
+    for i in range(0,n):
+        x[i] = (x[i] - x0) / 3600
+
+    return x
+
+def percentage(d, dn):
+    if isinstance(d, dict):
+        for key in d.keys():
+            d[key]['y'] = percentageArr(d[key]['y'], dn)
+    else:
+        return percentageArr(d, dn)
+        
+def percentageArr(y, dn):
+    n = np.size(y)
+
+    ymax = y[dn]
+    print 'ymax =  ' + str(ymax)
+    for i in range(0, n):
+        y[i] = y[i] / ymax
+
+    return y
+
+def integral(d, dn):
+    if isinstance(d, dict):
+        for key in d.keys():
+            d[key]['y'] = integralArr(d[key]['y'], dn)
+    else:
+        return integralArr(d, dn)
+        
+def integralArr(y, dn):
+    n = np.size(y)
+
+    for i in range(1, n):
+        y[i] = y[i] + y[i-1]
+
+    return y*10
+
+def toK(d):
+    if isinstance(d, dict):
+        for key in d.keys():
+            d[key]['y'] = toKArr(d[key]['y'])
+    else:
+        return toKArr(d)
+
+def toKArr(y):
+    n = np.size(y)
+        
+    for i in range(0, n):
+        y[i] = y[i] / 1000
+
+    return y
+
+def getInfluxQueryHarness(port, optsDict, utcstart, utcend):
+    return getInfluxQuery(port, optsDict, utcstart, utcend, etu.getHarnessHosts().split(' '))
+
+def getInfluxQuerySystem(port, optsDict, utcstart, utcend):
+    return getInfluxQuery(port, optsDict, utcstart, utcend, etu.getSystemHosts().split(' '))
+
+def getInfluxQuery(port, optsDict, utcstart, utcend, hosts):
+
+    #------------------------------------------------------------
+    # Construct value filter
+    #------------------------------------------------------------
+    
+    if optsDict['derivative']:
+        statFilter = "non_negative_derivative(sum(\"" + optsDict['stat'] + "\"),10s)/10"
+        groupByFilter = " group by time(10s) fill(0)"
+    else:
+        statFilter = "\"" + optsDict['stat'] + "\""
+        groupByFilter = ''
+
+    #------------------------------------------------------------
+    # Construct type filter
+    #------------------------------------------------------------
+
+    selectFilter = ''
+    print 'type Filter = ' + str(optsDict['typeFilter']) + ' select = ' + str(selectFilter)
+    
+    if optsDict['typeFilter'] != 'none':
+        if selectFilter == '':
+            selectFilter = " type = '" + optsDict['typeFilter'] + "'"
+        else:
+            selectFilter = " and type = '" + optsDict['typeFilter'] + "'"
+
+    #------------------------------------------------------------
+    # Construct type_instance filter
+    #------------------------------------------------------------
+
+    if optsDict['typeInstanceFilter'] != 'none':
+        if selectFilter == '':
+            selectFilter = " type_instance = '" + optsDict['typeInstanceFilter'] + "'"
+        else:
+            selectFilter += " and type_instance = '" + optsDict['typeInstanceFilter'] + "'"
+            
+    #------------------------------------------------------------
+    # Construct instance filter
+    #------------------------------------------------------------
+    
+    if optsDict['instanceFilter'] != 'none':
+        if selectFilter == '':
+            selectFilter = " instance = '" + optsDict['instanceFilter'] + "'"
+        else:
+            selectFilter += " and instance = '" + optsDict['instanceFilter'] + "'"
+
+    if selectFilter != '':
+        selectFilter += ' and '
+        
+    #------------------------------------------------------------        
+    # Construct the host filter
+    #------------------------------------------------------------
+    
+    nHost = np.size(hosts)
+    hostFilter = " ( host = '" + hosts[0] + "' "
+    for iHost in range(1, nHost):
+        hostFilter += "or host = '" + hosts[iHost] + "' "
+    hostFilter += ")"
+    query = "http://localhost:" + str(port) + "/query?db=collectd&q=select " + statFilter + " from " + optsDict['db'] + " where " + selectFilter + " time > '" + utcstart + "' and time < '" + utcend + "' and " + hostFilter + groupByFilter
+
+    print "Query = '" + query + "'"
+
+    r = requests.get(query)
+
+    j      = r.json()
+    res    = j['results']
+    series = res[0]['series']
+    vals   = series[0]['values']
+
+    return vals
+
+def parseDate(start, end, hoursafter, hoursbefore, lasthours):
+
+    # If last hours specified, this is time before now()
+    
+    if lasthours != None:
+        utcend   = datetime.datetime.utcnow()
+        utcstart = utcend - datetime.timedelta(hours=float(lasthours))
+
+    # Else if hoursafter was specified, this is time since utcstart
+    
+    elif hoursafter != None:
+        print 'after = ' + str(hoursafter)
+        utcstart = datetime.datetime.strptime(start, "%Y-%m-%dT%H:%M:%S.%fZ" )
+        utcend   = utcstart + datetime.timedelta(hours=float(hoursafter))
+    else:
+        utcend = datetime.datetime.strptime(end, "%Y-%m-%dT%H:%M:%S.%fZ" )
+        utcstart = utcend - datetime.timedelta(hours=float(hoursbefore))
+        
+    utcstart = utcstart.isoformat() + 'Z'
+    utcend   = utcend.isoformat() + 'Z'
+
+    print 'Parsing utcstart = ' + utcstart + ' end = ' + utcend + ' hours = ' + str(hoursafter)
+
+    return utcstart, utcend
+
+def storeXlim(ax, xlim, firstPlot):
+
+    newlim = ax.get_xlim()
+    retlim = []
+
+    if firstPlot:
+        firstPlot = False
+        retlim = newlim
+        print 'First plot: newlim = ' + str(newlim) + ' storing retlim = ' + str(retlim)
+    else:
+        retlim.append(etu.min(xlim[0], newlim[0]))
+        retlim.append(etu.max(xlim[1], newlim[1]))
+        ax.set_xlim(retlim)
+        print 'Not first plot: newlim = ' + str(newlim) + ' setting retlim = ' + str(retlim)
+        
+    return retlim, firstPlot
+
+def getAxes(plotWith, fileList, rt, statDict, overplot, pltsmooth, overplotsmooth):
+
+    # If overplotting all traces we only have one plot, unless we are
+    # also plotting smoothed versions and not overplotting those on
+    # top of the raw data, in which case we have 2
+
+    if overplot:
+        if pltsmooth and not overplotsmooth:
+            nPlot = 2
+        else:
+            nPlot = 1
+    elif pltsmooth and not overplotsmooth:
+        nPlot = 2*(np.size(fileDict.keys()) + np.size(statDict.keys()))
+    else:
+        nPlot = np.size(fileDict.keys()) + np.size(statDict.keys())
+
+    axs = []
+
+    for iPlot in range(0, nPlot):
+        axs.append(fig.add_subplot(nPlot, 1, iPlot+1))
+
+    return axs
+
+def getAxis(axs, iPlot, smooth, overplot, overplotsmooth):
+    if not smooth or overplotsmooth:
+        if overplot:
+            return axs[0]
+        else:
+            return axs[iPlot]
+    elif overplot:
+        return axs[1]
+    else:
+        nTrace = np.size(axs)/2
+        return axs[nTrace + iPlot]
+    
+def makePlot(ax, valDict, ind, relx, delta, xlim, firstPlot, smoothData, npt, perc, logy):
+
+    x = valDict[ind]['x']
+    y = valDict[ind]['y']
+    n = valDict[ind]['n']
+
+    if smoothData:
+        y = smooth(y, npt)
+
+    y = integralArr(y, delta)
+
+    if logy:
+        ax.set_yscale('log')
+
+    print 'Plotting data to axis ' + str(ax)
+    ax.plot(x[delta:n-delta], y[delta:n-delta])
+    xaxis = ax.get_xlim()
+
+    #newxlim = []
+    #newxlim.append(etu.min(xaxis[0], np.min(x[delta:n-delta])))
+    #newxlim.append(etu.max(xaxis[1], np.max(x[delta:n-delta])))
+    
+    if relx == False:
+        retick(plt, ax)
+
+    xlim, firstPlot = storeXlim(ax, xlim, firstPlot)
+
+    return xlim, firstPlot
+
+#=======================================================================
+# Script starts here
+#=======================================================================
+
+#------------------------------------------------------------
+# Arg parsing
+#------------------------------------------------------------
+
+figsize        = etu.strToIntTuple(etu.getOptArgs(sys.argv, 'figsize', '18,6'))
+pltsmooth      = etu.str2bool(etu.getOptArgs(sys.argv, 'smooth', 'false'))
+relx           = etu.str2bool(etu.getOptArgs(sys.argv, 'relx', 'false'))
+npt            = int(etu.getOptArgs(sys.argv, 'npt', '200'))
+rt             = etu.str2bool(etu.getOptArgs(sys.argv, 'rt', 'true'))
+overplot       = etu.str2bool(etu.getOptArgs(sys.argv, 'overplot', 'false'))
+overplotsmooth = etu.str2bool(etu.getOptArgs(sys.argv, 'overplotsmooth', 'true'))
+
+perc       = etu.str2bool(etu.getOptArgs(sys.argv, 'perc', 'false'))
+ports      = etu.strToIntArray(etu.getOptArgs(sys.argv, 'port', '58086'))
+dbs        = etu.getOptArgs(sys.argv, 'db', 'ycsb_operations').split(' ')
+stats      = etu.getOptArgs(sys.argv, 'stat', 'value').split(' ')
+typeFilters= etu.getOptArgs(sys.argv, 'type', 'none').split(' ')
+instanceFilters= etu.getOptArgs(sys.argv, 'instance', 'none').split(' ')
+typeInstanceFilters= etu.getOptArgs(sys.argv, 'type_instance', 'none').split(' ')
+harnesses  = etu.strToBoolArray(etu.getOptArgs(sys.argv, 'harness', 'true'))
+derivatives= etu.strToBoolArray(etu.getOptArgs(sys.argv, 'derivative', 'true'))
+starts     = etu.getOptArgs(sys.argv, 'start', '2017-02-07T19:50:06.939342Z').split(' ');
+end         = etu.getOptArgs(sys.argv, 'end',   '2017-02-07T20:55:06.000000Z');
+hoursbefore = etu.strToFloatArrayOrNone(etu.getOptArgs(sys.argv, 'hoursbefore', None))
+hoursafter  = etu.strToFloatArrayOrNone(etu.getOptArgs(sys.argv, 'hoursafter',  None))
+lasthours   = etu.getOptArgs(sys.argv, 'lasthours',   None)
+legend      = etu.toStrArrayOrNone(etu.getOptArgs(sys.argv, 'legend',   None), ';')
+xlimopt     = etu.getOptArgs(sys.argv, 'xlim',  None)
+
+nStat = np.size(ports)
+nStat = etu.max(nStat, np.size(starts))
+nStat = etu.max(nStat, np.size(dbs))
+nStat = etu.max(nStat, np.size(stats))
+nStat = etu.max(nStat, np.size(typeFilters))
+nStat = etu.max(nStat, np.size(typeInstanceFilters))
+nStat = etu.max(nStat, np.size(harnesses))
+nStat = etu.max(nStat, np.size(derivatives))
+
+if not rt:
+    nStat = 0
+    
+logys = etu.strToBoolArray(etu.getOptArgs(sys.argv, 'logy', 'false'))
+
+print 'rt = ' + str(rt)
+
+plotWith  = etu.getOptArgs(sys.argv, 'file', None)
+if plotWith != None:
+    plotWith = plotWith.split(' ')
+
+if pltsmooth:
+    dn  = npt
+else:
+    dn = 0
+
+#------------------------------------------------------------
+# Extract data from any files that were specified
+#------------------------------------------------------------
+
+nFile = np.size(plotWith)
+fileDict = {}
+fileList = []
+if plotWith != None:
+    for iFile in range(0, nFile):
+        fileName = plotWith[iFile]
+        lx, ly = etu.loadDataFromFile(fileName)
+        if relx:
+            lx = relative(lx)
+            ln = np.size(ly)
+        fileDict[fileName] = {}
+        fileDict[fileName]['file'] = fileName
+        fileDict[fileName]['x'] = lx
+        fileDict[fileName]['y'] = ly
+        fileDict[fileName]['n'] = ln
+        fileList.append(fileName)
+
+#    toK(fileDict)
+
+#------------------------------------------------------------
+# If overplotting real-time data, construct the query and retrieve it now
+#------------------------------------------------------------
+
+statDict = {}
+
+if rt:
+    for iStat in range(0, nStat):
+        optsDict = {}
+        
+        utcstart, utcend = parseDate(etu.indOrVal(starts, iStat), end, etu.indOrVal(hoursafter, iStat), etu.indOrVal(hoursbefore, iStat), lasthours)
+        harness    = etu.indOrVal(harnesses, iStat)
+
+        optsDict['stat']               = etu.indOrVal(stats, iStat)
+        optsDict['db']                 = etu.indOrVal(dbs, iStat)
+        optsDict['typeFilter']         = etu.indOrVal(typeFilters, iStat)
+        optsDict['instanceFilter']     = etu.indOrVal(instanceFilters, iStat)
+        optsDict['typeInstanceFilter'] = etu.indOrVal(typeInstanceFilters, iStat)
+        optsDict['derivative']         = etu.indOrVal(derivatives, iStat)
+
+        port       = etu.indOrVal(ports, iStat)
+        
+        if harness:
+            vals = getInfluxQueryHarness(port, optsDict, utcstart, utcend)
+        else:
+            vals = getInfluxQuerySystem(port, optsDict, utcstart, utcend)
+
+        f = open('influxQuery_' + str(iStat) + '.txt', 'wb')
+        
+        x = []
+        y = []
+        nv = np.size(vals,0)
+        for i in range(0, nv):
+
+            if vals[i][0].find('.') >= 0:
+                utc_dt = datetime.datetime.strptime(vals[i][0], "%Y-%m-%dT%H:%M:%S.%fZ")
+            else:
+                utc_dt = datetime.datetime.strptime(vals[i][0], "%Y-%m-%dT%H:%M:%SZ")
+                
+            xval = time.mktime(utc_dt.timetuple())
+            yval = vals[i][1]
+            x.append(xval)
+            y.append(yval)
+
+            f.write("%5.2f %5.2f\n" % (xval, yval))
+            
+        f.close()
+        
+        if relx:
+            x = relative(x)
+        n = np.size(x)
+
+        statDict[iStat] = {}
+        statDict[iStat]['x'] = x
+        statDict[iStat]['y'] = y
+        statDict[iStat]['n'] = n
+
+#------------------------------------------------------------
+# Make plots
+#------------------------------------------------------------
+
+fig = plt.figure(figsize=figsize)
+fig.set_facecolor('w')
+
+# If also plotting smoothed data, we will need two subplots
+
+if pltsmooth:
+    delta = dn
+else:
+    delta = 0
+
+#------------------------------------------------------------
+# Plot raw archival data first, then real-time data, if specified
+#------------------------------------------------------------
+
+if overplot:
+    nPlot = 1
+else:
+    nPlot = np.size(fileDict.keys()) + np.size(statDict.keys())
+
+if pltsmooth:
+    if not overplotsmooth:
+        nPlot = nPlot*2
+    
+iPlot = 1
+axs   = []
+
+firstPlot = True
+xlim = []
+
+print 'pltsmooth = ' + str(pltsmooth)
+
+axs   = getAxes(plotWith, fileList, rt, statDict, overplot, pltsmooth, overplotsmooth)
+nPlot = np.size(axs)
+
+#------------------------------------------------------------
+# Plot raw archival data first, then real-time data, if specified
+#------------------------------------------------------------
+
+iPlot = 0
+for key in fileList:
+    ax = getAxis(axs, iPlot, False, overplot, overplotsmooth)
+    xlim, firstPlot = makePlot(ax, fileDict, key, relx, delta, xlim, firstPlot, False, npt, False, False)
+    iPlot += 1
+
+for iStat in range(0, nStat):
+    ax = getAxis(axs, iPlot, False, overplot, overplotsmooth)
+    xlim, firstPlot = makePlot(ax, statDict, iStat, relx, delta, xlim, firstPlot, False, npt, False, etu.indOrVal(logys, iStat))
+    iPlot += 1
+        
+#------------------------------------------------------------
+# Plot smoothed archival data first, then real-time data, if specified
+#------------------------------------------------------------
+
+if pltsmooth:
+    
+    iPlot = 0
+    for key in fileList:
+        ax = getAxis(axs, iPlot, True, overplot, overplotsmooth)
+        xlim, firstPlot = makePlot(ax, fileDict, key, relx, delta, xlim, firstPlot, True, npt, perc, False)
+        iPlot += 1
+
+    for iStat in range(0, nStat):
+        ax = getAxis(axs, iPlot, True, overplot, overplotsmooth)
+        xlim, firstPlot = makePlot(ax, statDict, iStat, relx, delta, xlim, firstPlot, True, npt, perc, etu.indOrVal(logys, iStat))
+        iPlot += 1
+
+if legend != None:
+    plt.legend(legend, loc='upper left')
+    
+if relx:
+    plt.xlabel('Elapsed time (hours)')
+
+plt.ylabel('Integrated ops/sec')
+
+if xlimopt != None:
+    lims = plt.xlim()
+    newlims = []
+    newlims.append(lims[0])
+    newlims.append(float(xlimopt))
+    plt.xlim(newlims)
+    
+plt.show(block=True)
---
+++ python_scripts/kvlatency_cmp2.py
@@ -0,0 +1,295 @@
+import numpy as np
+#from matplotlib import rc
+#rc('text', usetex=True)
+#rc('text.latex', preamble='\usepackage{color}')
+import matplotlib.pyplot as plt
+import os
+import profiler_util as pu
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import sys
+
+getcolor='b'
+putcolor='r'
+delcolor='c'
+defFontSize=12
+
+def getLine(label, content):
+  for line in content:
+    if line.split(' ')[0] == label:
+      return line.split(' ')
+  return []
+
+def parseProfilerOutput(fileName, labelDict):
+
+  with open(fileName) as f:
+    content = f.readlines()
+
+    nline = len(content)
+
+    totalcount = getLine('totalcount', content)
+    labels     = getLine('label',      content)
+    counts     = getLine('count',      content)
+    usec       = getLine('usec',       content)
+
+  if len(labels) != 0:
+    for i in range(1, len(labels)):
+      label = labels[i].replace("'", "")
+      label = label.replace("\n", "")
+
+      if len(label) > 0:
+        labelDict[label] = {}
+        labelDict[label]['usec']  = float(usec[i+1])
+        labelDict[label]['count'] = int(counts[i+1])
+  return labelDict
+
+def getProfilerOutput(fileName):
+  d = pu.parseProfilerOutput(fileName, {})
+
+  gbytes = []
+  pbytes = []
+  dbytes = []
+
+  getus = []
+  getus_std = []
+
+  putus = []
+  putus_std = []
+
+  delus = []
+  delus_std = []
+
+  ret = {}
+  ret['hasd'] = False
+  
+  for key in d.keys():
+    s = key.split('_')
+    if s[0] == 'put':
+      pbytes.append(float(s[1]))
+      ntrial = int(s[2])
+      putus.append(d[key]['usec']/ntrial)
+      putus_std.append(d[key]['usecstd']/ntrial)
+    elif s[0] == 'get':
+      gbytes.append(float(s[1]))
+      ntrial = int(s[2])
+      getus.append(d[key]['usec']/ntrial)
+      getus_std.append(d[key]['usecstd']/ntrial)
+    elif s[0] == 'del':
+      ret['hasd'] = True
+      dbytes.append(float(s[1]))
+      ntrial = int(s[2])
+      delus.append(d[key]['usec']/ntrial)
+      delus_std.append(d[key]['usecstd']/ntrial)
+
+  npt = np.size(pbytes)
+  
+  btes = np.ndarray(npt)
+
+  puts = np.ndarray(npt)
+  gets = np.ndarray(npt)
+  dels = np.ndarray(npt)
+
+  puts_std = np.ndarray(npt)
+  gets_std = np.ndarray(npt)
+  dels_std = np.ndarray(npt)
+  
+  ps = np.argsort(pbytes)
+  gs = np.argsort(gbytes)
+
+  if ret['hasd']:
+    ds = np.argsort(dbytes)
+
+  for i in range(0,np.size(ps)):
+    btes[i] = pbytes[ps[i]]
+
+    puts[i] = putus[ps[i]]
+    gets[i] = getus[gs[i]]
+    if ret['hasd']:
+      dels[i] = delus[ds[i]]
+
+    puts_std[i] = putus_std[ps[i]]
+    gets_std[i] = getus_std[gs[i]]
+    if ret['hasd']:
+      dels_std[i] = delus_std[ds[i]]
+
+  ret['bytes'] = btes
+  ret['pv'] = puts
+  ret['ps'] = puts_std
+  ret['gv'] = gets
+  ret['gs'] = gets_std
+  ret['dv'] = dels
+  ret['ds'] = dels_std
+  
+  return ret
+
+def makePlot(ax, d, ls, xlabel=True, ylabel=True):
+  cstr = getcolor + ls
+  plt.plot(d['bytes'], d['gv'], cstr)
+  plt.hold(True)
+  cstr = putcolor + ls
+  plt.plot(d['bytes'], d['pv'], cstr)
+
+  if d['hasd']:
+    cstr = delcolor + ls
+    plt.plot(d['bytes'], d['dv'], cstr)
+
+  ax.set_ylim(100,1e6)  
+  ax.set_xscale('log')
+  ax.set_yscale('log')
+  
+  ax.xaxis.set_visible(False)
+  if xlabel:
+    plt.xlabel('Data size (bytes)',  fontsize=defFontSize)
+  if ylabel:
+    plt.ylabel('Latency ($\mu$sec)', fontsize=defFontSize)
+
+def makeDiffPlot(ax, d1, d2, args, ls, xlabel=True, ylabel=True):
+
+  bytes = d1['bytes']
+
+  print 'd1 = ' + str(d1['pv']) + ' d2 = ' + str(d2['pv'])
+  
+  puts = d2['pv'] / d1['pv']
+  puts_std = pu.getRatioSigma(d1['pv'], d1['ps'], d2['pv'], d2['ps'])
+
+  gets = d2['gv'] / d1['gv']
+  gets_std = pu.getRatioSigma(d1['gv'], d1['gs'], d2['gv'], d2['gs'])
+
+  if d1['hasd'] and d2['hasd']:
+    dels = d2['dv'] / d1['dv']
+    dels_std = pu.getRatioSigma(d1['dv'], d1['ds'], d2['dv'], d2['ds'])
+  
+  cstr = getcolor + ls
+  ax.plot(bytes,gets,cstr)
+  plt.hold(True)
+  cstr = putcolor + ls
+  plt.plot(bytes,puts,cstr)
+
+  if d1['hasd'] and d2['hasd']:
+    cstr = delcolor + ls
+    plt.plot(bytes,dels,cstr)
+
+  ax.set_xscale('log')
+  ax.set_ylim([0.4, 1.5])
+
+  grey=0.75
+  ax.fill_between(bytes, gets-gets_std, gets+gets_std, color=getcolor,edgecolor='none')
+  ax.fill_between(bytes, puts-puts_std, puts+puts_std, color=putcolor,edgecolor='none')
+
+  if d1['hasd'] and d2['hasd']:
+    ax.fill_between(bytes, dels-dels_std, dels+dels_std, color=delcolor,edgecolor='none')
+  
+  if xlabel:
+    plt.xlabel('Data size (bytes)',fontsize=defFontSize)
+  if ylabel:
+    plt.ylabel('Ratio',fontsize=defFontSize)
+
+  if args['rat'] != None:
+    plt.text(2.0, 1.3, '(' + args['rat'] + ')')
+
+def retick(ax, size):
+  ax.set_xticklabels(ax.get_xticklabels(), fontsize=size)
+  ax.set_yticklabels(ax.get_yticklabels(), fontsize=size)
+
+def makePanel(d1, d2, args, doX, doY):
+
+  ax = fig.add_subplot(1,3,args['pl'])
+  retick(ax, 14)
+
+  makePlot(ax, d1,  '-', doX, doY)
+  makePlot(ax, d2, '--', doX, doY)
+  
+  plt.legend(args['leg'], loc='upper left',fontsize=9)
+  plt.title(args['title'], fontsize=defFontSize)
+
+  divider = make_axes_locatable(ax)
+  axdiff  = divider.append_axes("bottom", 1.2, pad=0.1, sharex=ax)
+
+  makeDiffPlot(axdiff, d1, d2, args, '-', doX, doY)
+
+  return ax
+
+def getOptArgs(args, name, defval):
+    d = {}
+    for arg in args:
+      if '=' in arg:
+        splargs = arg.split('=')
+        d[splargs[0]] = splargs[1]
+
+
+    if name in d.keys():
+      return d[name]
+    else:
+      return defval
+
+dataDir = str(os.environ['RIAK_TEST_BASE']) + '/data/'
+
+usersuffix = getOptArgs(sys.argv, 'suffix', '')
+if usersuffix != '':
+  suffix = usersuffix + '_'
+else:
+  suffix = ''
+
+print 'Using suffix = ' + str(suffix)
+
+filesn1 = pu.getCmd('ls nval1_normal_' + suffix + 'iter*.txt').replace('\n', ' ')
+d1 = getProfilerOutput(filesn1)
+
+filesn3 = pu.getCmd('ls nval3_normal_' + suffix + 'iter*.txt').replace('\n', ' ')
+d3 = getProfilerOutput(filesn3)
+
+filesw1c1 = pu.getCmd('ls nval1_w1c_' + suffix + 'iter*.txt').replace('\n', ' ')
+dw1c1 = getProfilerOutput(filesw1c1)
+
+filesw1c3 = pu.getCmd('ls nval3_w1c_' + suffix + 'iter*.txt').replace('\n', ' ')
+dw1c3 = getProfilerOutput(filesw1c3)
+
+fig = plt.figure(figsize=(10,10))
+fig.set_facecolor('white');
+
+print 'Here 0'
+
+if suffix != '':
+  suffixstr=' (' + usersuffix + ') '
+else:
+  suffixstr=' '
+
+args = {}
+
+if d3['hasd']:
+  args['leg']   = ['nval=3 GET', 'nval=3 PUT', 'nval=3 DEL', 'nval=1 GET', 'nval=1 PUT', 'nval=1 DEL']
+else:
+  args['leg']   = ['nval=3 GET', 'nval=3 PUT', 'nval=1 GET', 'nval=1 PUT']
+    
+args['title'] = 'Riak KV' + suffixstr + 'Round-trip Latencies\nleveldb nval=3 vs. nval=1, erlang client'
+args['rat']   = 'nval=1/nval=3'
+args['pl']    = 1
+print 'Here 0'
+ax = makePanel(d3, d1, args, False, True)
+
+if dw1c3['hasd']:
+  args['leg']   = ['normal GET', 'normal PUT', 'normal DEL', 'w1c GET', 'w1c PUT', 'w1c DEL']
+else:
+  args['leg']   = ['normal GET', 'normal PUT', 'w1c GET', 'w1c PUT']
+  
+args['title'] = 'Riak KV' + suffixstr + 'Round-trip Latencies\nnormal vs. write-once, nval=3, erlang client'
+args['rat']   ='w1c/normal'
+args['pl']    = 2
+print 'Here 2'
+ax = makePanel(d3, dw1c3, args, True, True)
+
+if dw1c1['hasd']:
+  args['leg']   = ['normal GET', 'normal PUT', 'normal DEL', 'w1c GET', 'w1c PUT', 'w1c DEL']
+else:
+  args['leg']   = ['normal GET', 'normal PUT', 'w1c GET', 'w1c PUT']
+  
+args['title'] = 'Riak KV' + suffixstr + 'Round-trip Latencies\nnormal vs. write-once, nval=1, erlang client'
+args['rat']   ='w1c/normal'
+args['pl']    = 3
+print 'Here 2'
+ax = makePanel(d1, dw1c1, args, True, True)
+
+  
+fig.savefig('kvlatency_cmp2.png')
+plt.show()
+
+
